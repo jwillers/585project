0c642c9fe7ebb60d96c5795dbe8a555eabe9e7b6
jwillers <joel@msmmm.com>
2014-05-02 12:08:43 -0500

    Accumulation
    Added some comments
    Moved sections around in hopes of increasing efficiency
    Added accumulation functionality (not tested due to connectivity problem with Twitter)

--- App/server.R
+++ App/server.R
@@ -32,49 +32,55 @@ datalist <- head(parsedList,0)
 
 ## The main function for the map
 shinyServer(function(input, output, session) {
-# Take the data frame and make a reactive counterpart
+  # Take the data frame and make a reactive counterpart
   values<-reactiveValues()
-  values$datalist<-datalist
+  values$datalist <- datalist
   
   autoTime <- reactiveTimer(11000, session)
+  
+  # Make the map
+  output$worldMap <- renderPlot({
+    map('world')
+    # Make sure to show only when we have data
+    if (nrow(values$datalist)>0) {
+      # Make the points
+      points(values$datalist$place_lon, values$datalist$place_lat, pch = 21, bg="red", cex=2)
+    }
+  })
 
-observe ({
-  autoTime()
-  tempTweets <- filterStream( file.name="", track=input$contentText, timeout=9, oauth=my_oauth )
-  if(any(tempTweets == "Easy there, Turbo. Too many requests recently. Enhance your calm.")) {
-    parsedList <- data.frame("Session error, please wait")
-    Sys.sleep(3)
-  }
-  else if ((length(tempTweets)>0) & (any(tempTweets != "Exceeded connection limit for user"))) {
-    parsedList <- parseTweets(tempTweets)
-    parsedList <- subset(parsedList, !is.na(parsedList$place_lon))
-    values$datalist<-parsedList
-  }
-})  
-
-# Make the map
-output$worldMap <- renderPlot({
-  map('world')
-  # Make sure to show only when we have data
-  if (nrow(values$datalist)>0) {
-    # Make the points
-    points(values$datalist$place_lon, values$datalist$place_lat, pch = 21, bg="red", cex=2)
-  }
-})
-
-
-output$view <- renderTable({
-  if (nrow(values$datalist)>0) {
-    values$datalist
-  }
-  else data.frame("No results yet. You can wait or enter a new search term.")
-})
-
-
-
-
-
-
-
+  # Check the feed
+  observe ({
+    # First set this to run with autoTime
+    autoTime()
+    
+    # Check the stream
+    tempTweets <- filterStream( file.name="", track=input$contentText, timeout=9, oauth=my_oauth )
+    
+    # Test the stream for session errors and data. If there is data, parse it
+    if(any(tempTweets == "Easy there, Turbo. Too many requests recently. Enhance your calm.")) {
+      parsedList <- data.frame("Session error, please wait")
+      Sys.sleep(3)
+    }
+    else if ((length(tempTweets)>0) & (any(tempTweets != "Exceeded connection limit for user"))) {
+      parsedList <- parseTweets(tempTweets)
+      parsedList <- subset(parsedList, !is.na(parsedList$place_lon))
+      
+      # totalList has parsed tweets with locative coordinates
+      if (input$addData) {
+        values$datalist <- rbind(parsedList,isolate(values$datalist))
+      } else {
+        values$datalist <- parsedList
+      }
+    }
+  })  
+    
+  
+  output$view <- renderTable({
+    if (nrow(values$datalist)>0) {
+      values$datalist
+    }
+    else data.frame("No results yet. You can wait or enter a new search term.")
+  })
+  
   
 })
